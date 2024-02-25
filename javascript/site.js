077f55b5ca4e996d46ff94f2b4df6f9b4c9320f5
Nasa <157044833+NasaReabroi@users.noreply.github.com>
2024-02-24 20:20:50 -0600

    Add files via upload

---
+++ javascript/site.js
@@ -0,0 +1,26 @@
+alert('Welcome to Nasa Webpage !')
+
+document.addEventListener("DOMContentLoaded", function () {
+    var welcomeDiv = document.getElementById("welcome");
+    var hours = new Date().getHours();
+
+    var isMorning = hours >= 5 && hours < 12;
+    var isAfternoon = hours >= 12 && hours < 17;
+    var isEvening = hours >= 17 || hours < 5;
+
+    var welcomeMessage;
+
+    if (isMorning) {
+        welcomeMessage = "Good morning! Welcome to our site.";
+    } else if (isAfternoon) {
+        welcomeMessage = "Good afternoon! Welcome to our site.";
+    } else {
+        welcomeMessage = "Good evening! Welcome to our site.";
+    }
+
+    // Add the welcome message to the welcomeDiv
+    welcomeDiv.textContent = welcomeMessage;
+});
+
+
+localStorage.setItem("It's a secret to everybody.", "The only one person can identify by he/he is Michael Jackson");
